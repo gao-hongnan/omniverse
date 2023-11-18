6c15f99efd58688e39711b0b4de4730dd6aa1923
gao-hongnan <hongnangao@gmail.com>
2023-11-18 21:30:19 +0800

    dsa/searching_algorithms/client.py: add docstring.

--- omnixamples/dsa/searching_algorithms/client.py
+++ omnixamples/dsa/searching_algorithms/client.py
@@ -1,3 +1,16 @@
+"""
+This module provides a strategy design pattern implementation for searching
+products by ID in an inventory system. It supports two search strategies:
+linear search and binary search, chosen based on the specified traffic conditions.
+The module includes classes for representing products and inventory,
+a quicksort algorithm for sorting, and a command-line interface for user interaction.
+
+Example
+-------
+To run the script with command-line arguments:
+`python omnixamples/dsa/searching_algorithms/client.py --high-traffic --product-id 103`
+"""
+
 from argparse import ArgumentParser, Namespace
 from dataclasses import dataclass, field
 from typing import Callable, List
@@ -14,6 +27,7 @@ from omnivault.dsa.searching_algorithms.strategies import (
 
 @dataclass
 class Product:
+    """A class for representing a product."""
     id: int
     name: str
     price: float
@@ -21,25 +35,41 @@ class Product:
 
 @dataclass
 class Inventory:
+    """A class for representing an inventory of products."""
     items: List[Product] = field(default_factory=list)
 
     def add_new_item(self, product: Product) -> None:
+        """Adds a new product to the inventory."""
         self.items.append(product)
 
 
 def quicksort(items: List[Product], key: Callable[[Product], float]) -> List[Product]:
-    """Sorts a list of items using the quicksort algorithm based on a given key."""
+    """
+    Sort a list of products using the quicksort algorithm.
+
+    Parameters
+    ----------
+    items : List[Product]
+        A list of Product objects to be sorted.
+    key : Callable[[Product], float]
+        A function that extracts the sorting key from a Product object.
+
+    Returns
+    -------
+    List[Product]
+        The sorted list of products.
+    """
     if len(items) <= 1:
         return items
-    else:
-        pivot = items[0]
-        less_than_pivot = [item for item in items[1:] if key(item) <= key(pivot)]
-        greater_than_pivot = [item for item in items[1:] if key(item) > key(pivot)]
-        return (
-            quicksort(less_than_pivot, key)
-            + [pivot]
-            + quicksort(greater_than_pivot, key)
-        )
+
+    pivot = items[0]
+    less_than_pivot = [item for item in items[1:] if key(item) <= key(pivot)]
+    greater_than_pivot = [item for item in items[1:] if key(item) > key(pivot)]
+    return (
+        quicksort(less_than_pivot, key)
+        + [pivot]
+        + quicksort(greater_than_pivot, key)
+    )
 
 
 def find_product_by_id(
@@ -78,6 +108,7 @@ def parse_args() -> Namespace:
 
 
 def main() -> None:
+    """Main driver."""
     args = parse_args()
     # Creating an instance of Inventory
     inventory = Inventory()
@@ -101,6 +132,7 @@ def main() -> None:
 
 
 if __name__ == "__main__":
+    # python omnixamples/dsa/searching_algorithms/client.py --high-traffic --product-id 103
     found_product = main()
     if found_product:
         rich.print(f"Found product: {found_product.name}")
