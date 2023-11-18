17a85cc313bea500d121ec09490ce7719f23f94d
gao-hongnan <hongnangao@gmail.com>
2023-11-18 19:10:54 +0800

    refactor: minor update to code in dsa/ and also add newtype.py to omnivault/dsa/typings/.

--- omnivault/dsa/searching_algorithms/base.py
+++ omnivault/dsa/searching_algorithms/base.py
@@ -8,24 +8,31 @@ that are utilized by the concrete strategy classes in strategies.py and the
 context class in context.py.
 """
 
-# pylint:disable=all
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
-from typing import Protocol, Sequence
+from typing import Sequence, Literal
 
 from omnivault.dsa.typings.generics import Real
+from omnivault.dsa.typings.newtype import NonNegativeInt
 
 
 class Search(ABC):
+    """Base class for Search Strategies (Strategy Design Pattern)."""
+
     @abstractmethod
-    def search(self, container: Sequence[Real], target: Real) -> int:
-        ...
+    def search(
+        self, container: Sequence[Real], target: Real
+    ) -> NonNegativeInt | Literal[-1]:
+        """Searches for the target in the container and returns the index of the
+        target if found, otherwise returns -1."""
 
 
 class BinarySearch(Search):
     """Interface for Binary Search Strategies (Strategy Design Pattern)."""
 
     @abstractmethod
-    def mid_strategy(self, left: int, right: int) -> int:
+    def mid_strategy(
+        self, left: NonNegativeInt, right: NonNegativeInt
+    ) -> NonNegativeInt:
         """Strategy for calculating the middle index."""
