44d1fbd720af8608e95a8f5a287a4e484b598cd2
Hongnan Gao <hongnangao@gmail.com>
2023-12-15 22:50:21 +0800

    continuous-integration: update pre-commit config #28.

--- _tmp_types.py
+++ _tmp_types.py
@@ -8,6 +8,7 @@ import subprocess
 # Insecure use of subprocess with shell=True
 subprocess.call("echo 'Hello, world!'", shell=True)
 
+
 def append_any_to_stack(stack: StackList[Any], item: Any) -> StackList[Any]:
     stack.push(item)
     return stack
