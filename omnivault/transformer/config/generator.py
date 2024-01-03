f30802c6b5536d4a9113757fb10fe21efcdc146b
gao-hongnan <hongnangao@gmail.com>
2024-01-03 17:53:52 +0800

    omnivault: forgot to git add generator.py #11.

---
+++ omnivault/transformer/config/generator.py
@@ -0,0 +1,12 @@
+from typing import Union
+
+from pydantic import BaseModel, Field
+
+__all__ = ["GeneratorConfig"]
+
+
+class GeneratorConfig(BaseModel):
+    max_tokens: int = Field(default=1000)
+    temperature: float = Field(default=1.0, ge=0.0, le=1.0)
+    greedy: bool = Field(default=False)
+    top_k: Union[int, None] = Field(default=None)
