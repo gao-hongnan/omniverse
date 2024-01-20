8ef8311735352118091069dd0d6b814438e19fac
gao-hongnan <hongnangao@gmail.com>
2024-01-20 15:30:28 +0800

    omnivault/transformer: delete wiki2 example.

--- omnivault/transformer/projects/wiki2/main.py
+++
@@ -1,3 +0,0 @@
-"""The code for this module is deleted as I decided not to use it.
-Interested users can try Wikitext-103 instead which I have heard is better
-for language modelling. The old code can be found in the git history."""
