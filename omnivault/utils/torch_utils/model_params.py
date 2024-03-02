bb8cf4bd5ba171fa83e6435eadadbdb2b162c29c
gao-hongnan <hongnangao@gmail.com>
2024-03-02 21:11:53 +0800

    omniverse/transformer: add reversal notebook, and more content on decoder #41.

--- omnivault/utils/torch_utils/model_params.py
+++ omnivault/utils/torch_utils/model_params.py
@@ -1,8 +1,10 @@
 from torch import nn
 
+
 def total_trainable_parameters(module: nn.Module) -> int:
     """Returns the number of trainable parameters in the model."""
     return sum(p.numel() for p in module.parameters() if p.requires_grad)
+
 
 def total_parameters(module: nn.Module) -> int:
     """Returns the total number of parameters in the model, including non-trainable."""
