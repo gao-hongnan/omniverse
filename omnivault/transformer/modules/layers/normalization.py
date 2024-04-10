9b8e57ac7cad5827874b675a0f90566f1757aa00
gao-hongnan <hongnangao@gmail.com>
2024-04-10 12:20:03 +0800

    omnivault/omniverse: update docstring for normalization layer #81.

--- omnivault/transformer/modules/layers/normalization.py
+++ omnivault/transformer/modules/layers/normalization.py
@@ -30,35 +30,22 @@ class LayerNorm(nn.Module):
         See `PyTorch documentation <https://pytorch.org/docs/stable/generated/torch.nn.LayerNorm.html>`_
         for an example of how to apply `LayerNorm` across multiple dimensions.
 
-    Notation
-    --------
-    - :math:`\mathbf{Z}`: Input tensor of shape :math:`(\mathcal{B}, T, D)`.
-    - :math:`\mu`: Mean of the features to be normalized. Also denoted as :math:`\mathbb{E}[\mathbf{X}]`.
-    - :math:`\sigma`: Standard deviation of the features to be normalized.
-    - :math:`\gamma`: Learnable scaling parameter of shape :math:`(D_1, D_2, \ldots, D_n)`.
-    - :math:`\beta`: Learnable bias parameter of shape :math:`(D_1, D_2, \ldots, D_n)`.
-    - :math:`\epsilon`: Small constant added to the denominator for numerical stability.
-
     Formulation
     -----------
-    LayerNorm operates on the features defined by the `normalized_shape` dimensions.
-    It computes the mean and standard deviation of these features and applies
-    normalization and affine transformation.
-
-    The LayerNorm operation is defined as:
+    The LayerNorm operation for each feature dimension `d` in token `t` is defined as:
 
     .. math::
-        \text{LayerNorm}(\mathbf{X}) = \frac{\mathbf{X} - \mu}{\sqrt{\sigma^2 + \epsilon}} \cdot \gamma + \beta
+        \overline{\mathbf{Z}}_{td} = \frac{\mathbf{Z}_{td} - \mu_t}{\sqrt{\sigma_t^2 + \epsilon}} \cdot \gamma_d + \beta_d
 
     where:
-        - :math:`\mu` is the mean of the features to be normalized.
-        - :math:`\sigma` is the standard deviation of the features to be normalized.
-        - :math:`\gamma` is a learnable scaling parameter (if `elementwise_affine=True`).
-        - :math:`\beta` is a learnable bias parameter (if `elementwise_affine=True`).
+        - :math:`\mathbf{Z}_{td}` is the `d`-th feature of the token at position `t`.
+        - :math:`\mu_t` is the mean of the features for token `t`.
+        - :math:`\sigma_t^2` is the variance of the features for token `t`.
+        - :math:`\gamma_d` and :math:`\beta_d` are the scaling and shifting parameters for feature `d`.
         - :math:`\epsilon` is a small constant added for numerical stability.
 
-    The mean and standard deviation are computed across the `normalized_shape`
-    dimensions of the input tensor.
+    The mean :math:`\mu_t` and variance :math:`\sigma_t^2` are computed across
+    the features of each token, and normalization is applied independently to each token.
 
     Attributes
     ----------
@@ -89,12 +76,6 @@ class LayerNorm(nn.Module):
     >>> output = layer_norm(x)
     >>> output.shape
     torch.Size([2, 10, 128])
-
-    >>> layer_norm = LayerNorm((128, 256))
-    >>> x = torch.randn(2, 128, 256)  # Batch size 2, normalized dimensions 128 and 256
-    >>> output = layer_norm(x)
-    >>> output.shape
-    torch.Size([2, 128, 256])
     """
     __constants__ = ["normalized_shape", "eps", "elementwise_affine"]
 
@@ -164,86 +145,86 @@ class RMSNorm(nn.Module):
     -   :math:`\mathcal{B}`: Batch size.
     -   :math:`T`: Sequence length.
     -   :math:`D`: Number of features in the `d_model` dimension.
-    -   :math:`\mathbf{X}`: Input tensor of shape :math:`(\mathcal{B}, T, D)`.
-    -   :math:`\mathbf{x}`: One sequence in the batch consisting of :math:`T`
+    -   :math:`\mathbf{Z}^{\mathcal{B}}`: Input tensor of shape :math:`(\mathcal{B}, T, D)`.
+    -   :math:`\mathbf{Z}`: One sequence in the batch consisting of :math:`T`
         tokens, of shape :math:`(1, T, D)` or simply :math:`(T, D)` when considering
         it independently.
-    -   :math:`\mathbf{x}_t`: A single vector/token of `D` features at position `t`
+    -   :math:`\mathbf{Z}_t`: A single vector/token of `D` features at position `t`
         in the input sequence, of shape :math:`(1, D)` or simply :math:`(D,)`.
-    -   :math:`x_{td}`: `d`-th element of the vector :math:`\mathbf{x}_t`, where
+    -   :math:`Z_{td}`: `d`-th element of the $t$-th token vector :math:`\mathbf{Z}_t`, where
         :math:`d \in \{1, 2, \ldots, D\}`.
-    -   :math:`\text{RMS}(\mathbf{x}_t)`: Root mean square of the vector
-        :math:`\mathbf{x}_t`, applied element-wise.
+    -   :math:`\text{RMS}(\mathbf{Z}_t)`: Root mean square of the vector
+        :math:`\mathbf{Z}_t`, applied element-wise.
     -   :math:`\gamma`: Learnable scaling parameter of shape :math:`(D,)`.
     -   :math:`\epsilon`: Small constant added to the denominator for numerical
         stability.
 
     Formulation
     -----------
-    Given an input tensor :math:`\mathbf{X}` of shape :math:`(\mathcal{B}, T, D)`, where
-    :math:`\mathcal{B}` represents the batch size, :math:`T` the sequence length, and
-    :math:`D` the number of features in the `d_model` dimension, the RMSNorm operation
-    applies as follows for each vector :math:`\mathbf{x}_t` within :math:`\mathbf{X}`,
-    at each position :math:`t` within a sequence, and for each sequence within the
+    Given a batch of input tensor :math:`\mathbf{Z}^{\mathcal{B}}` of shape
+    :math:`(\mathcal{B}, T, D)`, where :math:`\mathcal{B}` represents the batch
+    size, :math:`T` the sequence length, and :math:`D` the number of features in the
+    `d_model` dimension, the RMSNorm operation applies as follows for each vector
+    :math:`\mathbf{Z}_t` within each sequence :math:`\mathbf{Z}` where
+    :math:`\mathbf{Z} \in \mathbf{Z}^{\mathcal{B}}`. In other words, the
+    normalization is applied independently on :math:`\mathbf{Z}_t` at each position
+    :math:`t` within a sequence :math:`\mathbf{Z}`, and for each sequence within the
     batch.
 
-    The RMSNorm operation for an activation vector :math:`\mathbf{x}_t`, which is a
-    part of the input batched tensor :math:`\mathbf{X}`, is defined by:
+    The RMSNorm operation for an activation vector :math:`\mathbf{Z}_t` is
+    defined by:
 
     .. math::
-        \text{RMSNorm}(\mathbf{x}_t) = \frac{\mathbf{x}_t}{\text{RMS}(\mathbf{x}_t)} \cdot \gamma
+        \text{RMSNorm}(\mathbf{Z}_t) = \frac{\mathbf{Z}_t}{\text{RMS}(\mathbf{Z}_t)} \odot \gamma
 
-    Here, :math:`\text{RMS}(\mathbf{x}_t)` computes the root mean square of the
-    vector :math:`\mathbf{x}_t`, and is defined as:
+    Here, :math:`\text{RMS}(\mathbf{Z}_t)` computes the root mean square of the
+    vector :math:`\mathbf{Z}_t`, and is defined as:
 
     .. math::
-        \text{RMS}(\mathbf{x}_t) = \sqrt{\frac{1}{D} \sum_{d=1}^{D} x_{td}^2 + \epsilon}
+        \text{RMS}(\mathbf{Z}_t) = \sqrt{\frac{1}{D} \sum_{d=1}^{D} Z_{td}^2 + \epsilon}
 
-    - :math:`x_{td}` denotes the :math:`d`-th element of the vector :math:`\mathbf{x}_t`,
-      with :math:`d` ranging from 1 to :math:`D`, reflecting the dimensionality of
-      the feature space.
+    - :math:`Z_{td}` denotes the :math:`d`-th element of the vector :math:`\mathbf{Z}_t`,
+      at time step `t`, with :math:`d` ranging from 1 to :math:`D`, which is
+      the dimensionality of the feature space.
     - :math:`\gamma` is a learnable scaling parameter vector of shape :math:`(D,)`,
       allowing adaptive scaling of the normalized activation vector. It is applied
       element-wise in conjunction with the normalization factor.
     - :math:`\epsilon` is a small constant added for numerical stability, often set
       to a value like :math:`1e-5`.
 
-    The element-wise computation for each feature :math:`x_{td}` in the normalized
-    vector :math:`\text{RMSNorm}(\mathbf{x}_t)` can be expressed as:
+    The element-wise computation for each feature :math:`Z_{td}` in the normalized
+    vector :math:`\text{RMSNorm}(\mathbf{Z}_t)` can be expressed as:
 
     .. math::
-        \text{RMSNorm}(x_{td}) = \frac{x_{td}}{\sqrt{\frac{1}{D} \sum_{d=1}^{D} x_{td}^2 + \epsilon}} \cdot \gamma_d
+        \text{RMSNorm}(Z_{td}) = \frac{Z_{td}}{\sqrt{\frac{1}{D} \sum_{d=1}^{D} Z_{td}^2 + \epsilon}} \cdot \gamma_d
 
     where :math:`\gamma_d` represents the :math:`d`-th element of the learnable
-    scaling parameter vector :math:`\gamma`. This is a useful interpretation
+    scaling parameter vector :math:`\gamma`. This is an useful interpretation
     even though we view the normalization as being applied to the entire vector
-    :math:`\mathbf{x}_t` at once.
+    :math:`\mathbf{Z}_t` at once.
 
-    Notes
-    -----
-    Like `LayerNorm`, `RMSNorm` normalizes the inputs across the feature dimension
-    (i.e., `d`), but it uses the root mean square (RMS) of the elements
-    instead of the standard deviation.
+    .. note::
+        Like `LayerNorm`, `RMSNorm` normalizes the inputs across the feature dimension
+        (i.e., :math:`D`), but it uses the root mean square (RMS) of the elements
+        instead of the standard deviation.
 
-    In other words, if an input tensor `x` has shape `(B, T, D)`, then `RMSNorm` is
-    applied across the `D` dimension for each position/token `x_t` (shape `1 x D`)
-    in the sequence. This means that the normalization is applied independently
-    for each position in the sequence and if `B=2, T=3, D=4`, this means that
-    the normalization is applied to `B x T = 6` vectors (tokens) of shape `1 x 4`.
+        If an input tensor :math:`\mathbf{Z}^{\mathcal{B}}` has shape :math:`(B, T, D)`, then `RMSNorm` is
+        applied across the `D` dimension for each position/token :math:`\mathbf{Z}_t`
+        (shape :math:`1 \times D`) in the sequence. This means that the normalization is
+        applied independently for each position in the sequence and, for example, if
+        :math:`B=2, T=3, D=4`, this means that the normalization is applied to
+        :math:`B \times T = 6` vectors (tokens) of shape :math:`1 \times 4`.
 
-    Parameters
+    Attributes
     ----------
-    d : int
-        The number of features in the `d` dimension.
+    d_model : int
+        The size of the hidden dimension `d_model`.
     eps : float, optional
         A small constant added to the denominator for numerical stability. Default: 1e-5.
     device : torch.device or str, optional
         The device on which the module will be allocated. Default: None.
     dtype : torch.dtype, optional
         The data type for the module's parameters. Default: None.
-
-    Attributes
-    ----------
     gain : torch.nn.Parameter
         The learnable scaling parameter of shape `(D,)`.
     normalized_shape : Tuple[int]
@@ -265,15 +246,6 @@ class RMSNorm(nn.Module):
         device: Optional[Union[_device, str, None]] = None,
         dtype: Optional[_dtype] = None,
     ) -> None:
-        """
-        Initializes the RMSNorm module.
-
-        Args:
-            d_model (int): The number of features in the `d_model` dimension.
-            eps (float, optional): A small constant added to the denominator for numerical stability. Default: 1e-5.
-            device (torch.device or str, optional): The device on which the module will be allocated. Default: None.
-            dtype (torch.dtype, optional): The data type for the module's parameters. Default: None.
-        """
         super().__init__()
 
         factory_kwargs = {"device": device, "dtype": dtype}
@@ -308,7 +280,7 @@ class RMSNorm(nn.Module):
         x_normalized_affine_BTD: torch.Tensor
             Normalized tensor of the same shape as the input tensor.
         """
-        # x is usually of shape `[B, T, D]` so `D` is the last dimension
+        # x is of shape `[B, T, D]` so `D` is the last dimension
         x_root_mean_squared_BT1 = torch.sqrt(torch.sum(x**2, dim=-1, keepdim=True) / self.d_model + self.eps)
         x_normalized_BTD = x / x_root_mean_squared_BT1
         x_normalized_affine_BTD = x_normalized_BTD * self.gain
