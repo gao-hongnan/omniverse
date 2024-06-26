d2584cbd1fcf36147c1a40cda25e6c5dcc0b7bce
gao-hongnan <hongnangao@gmail.com>
2024-06-26 16:16:00 +0800

    feat: add model freezing [#87].

--- omnivault/utils/torch_utils/model_utils.py
+++ omnivault/utils/torch_utils/model_utils.py
@@ -30,12 +30,15 @@ def total_parameters(module: nn.Module) -> int:
     return sum(p.numel() for p in module.parameters())
 
 
-def get_named_modules(model: nn.Module, **kwargs: Any) -> List[Dict[str, str]]:
-    """Obtain a list of named modules in the model.
+def get_named_modules(module: nn.Module, **kwargs: Any) -> List[Dict[str, str]]:
+    """Obtain a list of named modules in the model/module.
+
+    Return an iterator over all modules in the network, yielding both the name
+    of the module as well as the module itself.
 
     Parameters
     ----------
-    model : nn.Module
+    module : nn.Module
         The model to extract named modules from.
     **kwargs : Any
         Additional keyword arguments to pass to the `named_modules` method.
@@ -50,21 +53,173 @@ def get_named_modules(model: nn.Module, **kwargs: Any) -> List[Dict[str, str]]:
     >>> from torchvision.models import resnet18, ResNet18_Weights
     >>> backbone = resnet18(weights=ResNet18_Weights.DEFAULT)
     >>> named_modules = get_named_modules(backbone)
+
+    ```python
+    some_module = nn.ModuleDict(
+        {"backbone": nn.Sequential(
+            nn.Embedding(2, 2),
+        ),
+        "pooler":
+            nn.Sequential(
+                nn.Linear(1, 2),
+            ),
+        "head": nn.Sequential(
+            nn.Conv2d(1, 3, 1),
+        )
+    })
+    ```
+
+    The model definition is:
+
+    ```python
+    ModuleDict(
+    (backbone): Sequential(
+        (0): Embedding(2, 2)
+    )
+    (pooler): Sequential(
+        (0): Linear(in_features=1, out_features=2, bias=True)
+    )
+    (head): Sequential(
+        (0): Conv2d(1, 3, kernel_size=(1, 1), stride=(1, 1))
+    )
+    )
+    ```
+
+    Then if we call `get_named_modules(some_module)`, we will get the following output:
+    ```python
+    [
+        {
+            "": "ModuleDict(backbone): Sequential((0): Embedding(2, 2))pooler): Sequential((0): Linear(in_features=1, out_features=2, bias=True))head): Sequential((0): Conv2d(1, 3, kernel_size=(1, 1), stride=(1, 1)))\n)"
+        },
+        {"backbone": "Sequential(0): Embedding(2, 2)\n)"},
+        {"backbone.0": "Embedding(2, 2)"},
+        {"pooler": "Sequential(0): Linear(in_features=1, out_features=2, bias=True)\n)"},
+        {"pooler.0": "Linear(in_features=1, out_features=2, bias=True)"},
+        {"head": "Sequential(0): Conv2d(1, 3, kernel_size=(1, 1), stride=(1, 1))\n)"},
+        {"head.0": "Conv2d(1, 3, kernel_size=(1, 1), stride=(1, 1))"},
+    ]
+    ```
     """
     named_modules = []
-    for module in model.named_modules(**kwargs):
-        module_name, module_type = module
+    for _module in module.named_modules(**kwargs):
+        module_name, module_type = _module
         named_modules.append({str(module_name): str(module_type)})
     return named_modules
 
 
-def gather_weight_stats(model: nn.Module, **kwargs: Any) -> Dict[str, Dict[str, float]]:
+def get_named_parameters(module: nn.Module, **kwargs: Any) -> List[Dict[str, nn.Parameter]]:
+    """Obtain a list of named parameters in the model/module.
+
+    Return an iterator over all parameters in the network, yielding both the name
+    of the parameter as well as the parameter itself.
+
+    Parameters
+    ----------
+    module : nn.Module
+        The model to extract named parameters from.
+    **kwargs : Any
+        Additional keyword arguments to pass to the `named_parameters` method.
+
+    Returns
+    -------
+    List[Dict[str, nn.Parameter]]
+        A list of dictionaries containing the name and parameter of each parameter in the model.
+
+    Examples
+    --------
+    >>> from torchvision.models import resnet18, ResNet18_Weights
+    >>> backbone = resnet18(weights=ResNet18_Weights.DEFAULT)
+    >>> named_parameters = get_named_parameters(backbone)
+
+    ```python
+    some_module = nn.ModuleDict(
+        {"backbone": nn.Sequential(
+            nn.Embedding(2, 2),
+        ),
+        "pooler":
+            nn.Sequential(
+                nn.Linear(1, 2),
+            ),
+        "head": nn.Sequential(
+            nn.Conv2d(1, 3, 1),
+        )
+    })
+    ```
+
+    The model definition is:
+
+    ```python
+    ModuleDict(
+    (backbone): Sequential(
+        (0): Embedding(2, 2)
+    )
+    (pooler): Sequential(
+        (0): Linear(in_features=1, out_features=2, bias=True)
+    )
+    (head): Sequential(
+        (0): Conv2d(1, 3, kernel_size=(1, 1), stride=(1, 1))
+    )
+    )
+    ```
+
+    Then if we call `get_named_parameters(some_module)`, we will get the following output:
+
+    ```text
+    [
+        {
+            'backbone.0.weight': Parameter containing:
+            tensor([
+                [ 0.4833, -0.9517],
+                [ 0.7517, -0.3315]
+            ], requires_grad=True)
+        },
+        {
+            'pooler.0.weight': Parameter containing:
+            tensor([
+                [ 0.8330],
+                [-0.0932]
+            ], requires_grad=True)
+        },
+        {
+            'pooler.0.bias': Parameter containing:
+            tensor([
+                -0.5987,
+                0.2774
+            ], requires_grad=True)
+        },
+        {
+            'head.0.weight': Parameter containing:
+            tensor([
+                [[[-0.3349]]],
+                [[[-0.4394]]],
+                [[[ 0.2738]]]
+            ], requires_grad=True)
+        },
+        {
+            'head.0.bias': Parameter containing:
+            tensor([
+                0.5669,
+                0.7181,
+                -0.0820
+            ], requires_grad=True)
+        }
+    ]
+    ```
+    """
+    named_parameters = []
+    for _parameter in module.named_parameters(**kwargs):
+        parameter_name, parameter_type = _parameter
+        named_parameters.append({str(parameter_name): parameter_type})
+    return named_parameters
+
+
+def gather_weight_stats(module: nn.Module, **kwargs: Any) -> Dict[str, Dict[str, float]]:
     """Return the mean and standard deviation of weights and biases in the model. Sanity
     check to ensure that the weights and biases are initialized correctly.
 
     Parameters
     ----------
-    model : nn.Module
+    module : nn.Module
         The model to extract weight statistics from.
     **kwargs : Any
         Additional keyword arguments to pass to the `named_modules` method.
@@ -84,8 +239,8 @@ def gather_weight_stats(model: nn.Module, **kwargs: Any) -> Dict[str, Dict[str, 
     >>> plot_distribution_stats(stats_df, layer_type="Conv2d")
     """
     stats = {}
-    for module in model.named_modules(**kwargs):
-        module_name, module_type = module
+    for _module in module.named_modules(**kwargs):
+        module_name, module_type = _module
         assert isinstance(module_type, nn.Module)
         if (
             hasattr(module_type, "weight")
@@ -210,3 +365,108 @@ def compare_models_and_report_differences(model_a: nn.Module, model_b: nn.Module
         return False, differences
     else:
         return True, None
+
+
+class Freezer:
+    """A utility class for freezing and unfreezing parameters in a PyTorch model.
+
+    This class allows for freezing the parameters of a model's layers either by
+    the index of the layer or by the name of the parameters. It keeps track of
+    the freeze state of each parameter, allowing for selective training of model parts.
+
+    Examples
+    --------
+    >>> from rich.pretty import pprint
+    >>> from torch import nn
+    >>> model = nn.Sequential(
+    ...     nn.Conv2d(1, 20, 5, bias=False),
+    ...     nn.Conv2d(20, 50, 5, bias=False),
+    ...     nn.Linear(800, 500, bias=False),
+    ...     nn.Linear(500, 10, bias=False),
+    ... )
+    >>> freezer = Freezer(model)
+    >>> freezer.freeze_by_index([0, 2])
+    >>> status = freezer.report_freezing()
+    >>> pprint(status)
+    {'0.weight': True, '1.weight': False, '2.weight': True, '3.weight': False}
+
+    >>> assert model[0].weight.requires_grad is False
+    >>> assert model[1].weight.requires_grad is True
+    >>> assert model[2].weight.requires_grad is False
+    >>> assert model[3].weight.requires_grad is True
+
+    >>> freezer.freeze_by_name(["3.weight"])
+    >>> status = freezer.report_freezing()
+    >>> pprint(status)
+    {'0.weight': True, '1.weight': False, '2.weight': True, '3.weight': True}
+    """
+
+    def __init__(self, model: nn.Module) -> None:
+        """Initialize the Freezer with the model. Note given the nature of mutable
+        objects, the `model` is a reference to the original model from outside the
+        constructor, and any modifications to the model will be reflected inplace.
+        """
+        self.model = model
+        self.frozen_status: Dict[str, bool] = {}
+        self.update_freezing_status()
+
+    def update_freezing_status(self) -> None:
+        """
+        Update the freezing status of all parameters in the model.
+        """
+        for name, param in self.model.named_parameters():
+            self.frozen_status[name] = not param.requires_grad
+
+    def freeze_by_index(self, indices: List[int]) -> None:
+        """
+        Freeze layers based on their index in the ordered list of model's children.
+
+        Parameters
+        ----------
+        indices : List[int]
+            List of indices of the layers to freeze.
+        """
+        for idx, child in enumerate(self.model.children()):
+            if idx in indices:
+                for param in child.parameters():
+                    param.requires_grad = False
+        self.update_freezing_status()
+
+    def freeze_by_name(self, names: List[str]) -> None:
+        """
+        Freeze layers based on the names of the parameters.
+
+        Parameters
+        ----------
+        names : List[str]
+            List of substrings to match in the parameter names for freezing.
+            This name can be obtained by calling `model.named_parameters()`.
+        """
+        for name, param in self.model.named_parameters():
+            if any(n in name for n in names):
+                param.requires_grad = False
+        self.update_freezing_status()
+
+    def report_freezing(self) -> Dict[str, bool]:
+        """
+        Report the freezing status of all parameters in the model.
+
+        Returns
+        -------
+        Dict[str, bool]
+            Dictionary with parameter names and their freezing status.
+        """
+        return self.frozen_status
+
+
+def freeze_layers(module: nn.Module) -> None:
+    """Freezes the specified number of layers in the backbone. See `Freezer` for
+    a more comprehensive way to freeze layers in a model.
+
+    Parameters
+    ----------
+    module : nn.Module
+        The model to freeze the layers in.
+    """
+    for parameter in module.parameters():
+        parameter.requires_grad = False
